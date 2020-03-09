6b56f5235537c8c49c42eee11cdcdd3fef8a41a8
Dario Panada <dario.panada@manchester.ac.uk>
2020-03-09 14:45:05 +0000

    Remort SourceSinkWatcher

--- model/helpers/OxygenSourceSinkWatcher.py
+++ model/helpers/OxygenSourceSinkWatcher.py
@@ -1,13 +1,11 @@
 from panaxea.core.Steppables import Helper
 
-'''
-Will store the source/sink coordinates for oxygen as part of the model 
-properties
-'''
-
 
 class OxygenSourceSinkWatcher(object, Helper):
     '''
+    Will store the source/sink coordinates for oxygen as part of the model
+    properties
+
     :param model - The main model object
     :param oxygenDiffusionHelper - A reference to the oxygen diffusion helper
     :param interval - (Optional - defaults to 10), will store every
@@ -16,19 +14,16 @@ class OxygenSourceSinkWatcher(object, Helper):
 
     def __init__(self, model, oxygenDiffusionHelper, interval=10):
         model.output["oxygenGrids"] = []
-        self.oxygenDiffusionHelper = oxygenDiffusionHelper
+        self.oxygen_diffusion_helper = oxygenDiffusionHelper
         self.interval = interval
 
-    '''
-    Diffusion runs in the prologue, so we capture in the epilogue.
-    '''
-
     def step_epilogue(self, model):
+        # Diffusion runs in the prologue, so we capture in the epilogue.
         if model.current_epoch % self.interval == 0:
-            sourceCoords = self.oxygenDiffusionHelper.sourceCoords
-            sinkCoords = self.oxygenDiffusionHelper.sinkCoords
+            source_coords = self.oxygen_diffusion_helper.sourceCoords
+            sink_coords = self.oxygen_diffusion_helper.sinkCoords
             model.output["oxygenGrids"].append({
                 "epoch": model.current_epoch,
-                "sourceCoords": sourceCoords,
-                "sinkCoords": sinkCoords
+                "sourceCoords": source_coords,
+                "sinkCoords": sink_coords
             })
