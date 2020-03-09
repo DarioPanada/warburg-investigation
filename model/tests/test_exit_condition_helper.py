a253cd8b74c1f4c05ddf449fc11bca41b837507e
Dario Panada <dario.panada@manchester.ac.uk>
2020-03-09 15:25:50 +0000

    Reformat exit condition helper

--- model/tests/test_exit_condition_helper.py
+++ model/tests/test_exit_condition_helper.py
@@ -1,13 +1,12 @@
 import unittest
-
 from core.Model import Model
-from panaxea.core.Steppables import Agent
 from models.alpha04c.helpers.ExitConditionWatcher import ExitConditionWatcher
+from panaxea.core.Steppables import Agent
 
-class testExitCondition(unittest.TestCase):
+
+class TestExitCondition(unittest.TestCase):
 
     def test_exit_conditions(self):
-
         class TickerAgent(Agent, object):
 
             def __init__(self):
@@ -20,14 +19,16 @@ class testExitCondition(unittest.TestCase):
         model = Model(epochs, verbose=False)
         model.schedule.agents.add(TickerAgent())
 
-        def tickerCondition(model):
-            expiredTickers = [a for a in model.schedule.agents if a.counter >=4]
-            return len(expiredTickers) > 0
+        def ticker_condition(model):
+            expired_tickers = [a for a in model.schedule.agents if
+                               a.counter >= 4]
+            return len(expired_tickers) > 0
 
-        model.schedule.helpers.append(ExitConditionWatcher([tickerCondition]))
+        model.schedule.helpers.append(ExitConditionWatcher([ticker_condition]))
 
         model.run()
         self.assertEqual(model.current_epoch, 4)
+
 
 if __name__ == '__main__':
     unittest.main()
