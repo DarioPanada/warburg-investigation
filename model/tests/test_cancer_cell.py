b77b287768566a7f5b1a28e037219917e9077277
Dario Panada <dario.panada@manchester.ac.uk>
2020-03-09 15:24:16 +0000

    Reformat cancer cell

--- model/tests/test_cancer_cell.py
+++ model/tests/test_cancer_cell.py
@@ -1,5 +1,5 @@
-import unittest
 import numpy as np
+import unittest
 from core.Environment import NumericalGrid3D, ObjectGrid3D
 from core.Model import Model
 from models.alpha04c.agents.CancerCell import CancerCell
@@ -17,25 +17,26 @@ class testCancerCell(unittest.TestCase):
 
         # VEGF secretion rate should increase as HIF concentration increases
         delta = 0.1
-        for h in np.arange(2,15, delta):
-            a.currentHifRate = h
+        for h in np.arange(2, 15, delta):
+            a.current_hif_rate = h
             a.__update_vegf_secretion_rate()
-            vegfRate = a.currentVegfSecretionRate
+            vegf_rate = a.current_vegf_secretion_rate
 
-            a.currentHifRate = h+delta
+            a.current_hif_rate = h + delta
             a.__update_vegf_secretion_rate()
-            self.assertGreater(a.currentVegfSecretionRate, vegfRate)
-            self.assertLessEqual(0, a.currentVegfSecretionRate)
-            self.assertGreaterEqual(a.currentVegfSecretionRate, vegfRate)
+            self.assertGreater(a.current_vegf_secretion_rate, vegf_rate)
+            self.assertLessEqual(0, a.current_vegf_secretion_rate)
+            self.assertGreaterEqual(a.current_vegf_secretion_rate, vegf_rate)
 
-        # Probability of progressing into synthesis should increase as hif concentration increases
+        # Probability of progressing into synthesis should increase as hif
+        # concentration increases
         delta = 0.1
-        for h in np.arange(2,15, delta):
-            a.currentHifRate = h
+        for h in np.arange(2, 15, delta):
+            a.current_hif_rate = h
             a.__update_p_synthesis()
             pSyntheis = a.currentPSynthesis
 
-            a.currentHifRate = h+delta
+            a.current_hif_rate = h + delta
             a.__update_p_synthesis()
             self.assertGreaterEqual(a.currentPSynthesis, pSyntheis)
             self.assertLessEqual(0, a.currentPSynthesis)
@@ -43,46 +44,53 @@ class testCancerCell(unittest.TestCase):
 
         # Metabolic rate should decreases as hif concentration increase
         delta = 0.1
-        for h in np.arange(2,15, delta):
-            a.currentHifRate = h
+        for h in np.arange(2, 15, delta):
+            a.current_hif_rate = h
             a.__update_metabolic_rate()
-            mr = a.currentMetabolicRate
-            a.currentHifRate = h+delta
+            mr = a.current_metabolic_rate
+            a.current_hif_rate = h + delta
             a.__update_metabolic_rate()
-            self.assertGreaterEqual(mr, a.currentMetabolicRate)
-            self.assertLessEqual(0, a.currentMetabolicRate)
-            self.assertGreaterEqual(1, a.currentMetabolicRate)
+            self.assertGreaterEqual(mr, a.current_metabolic_rate)
+            self.assertLessEqual(0, a.current_metabolic_rate)
+            self.assertGreaterEqual(1, a.current_metabolic_rate)
 
-        # Expectation is that HIF expression rate increases as oxygen decreases, until again falling sharply
-        for o in np.arange(0.2,1, 0.1):
-            self.assertEquals(a.__calculate_hif_expression_rate_from_oxygen(o), 1)
+        # Expectation is that HIF expression rate increases as oxygen
+        # decreases, until again falling sharply
+        for o in np.arange(0.2, 1, 0.1):
+            self.assertEquals(a.__calculate_hif_expression_rate_from_oxygen(o),
+                              1)
 
         delta = 0.1
         # HIF expression increases between 0.2 and ~0.02
         for o in np.arange(0.02, 0.2, delta):
-            self.assertGreaterEqual(a.__calculate_hif_expression_rate_from_oxygen(o), a.__calculate_hif_expression_rate_from_oxygen(o + delta))
+            self.assertGreaterEqual(
+                a.__calculate_hif_expression_rate_from_oxygen(o),
+                a.__calculate_hif_expression_rate_from_oxygen(o + delta))
 
         # HIF expression decreases from ~0.02
-        for o in np.arange(0,0.02, delta):
-            self.assertGreaterEqual(a.__calculate_hif_expression_rate_from_oxygen(o + delta), a.__calculate_hif_expression_rate_from_oxygen(o))
+        for o in np.arange(0, 0.02, delta):
+            self.assertGreaterEqual(
+                a.__calculate_hif_expression_rate_from_oxygen(o + delta),
+                a.__calculate_hif_expression_rate_from_oxygen(o))
 
-        oxygenEnvName = "oxygenEnv"
-        agentEnvName = "agentEnv"
-        model.properties["envNames"]["oxygenEnvName"] = oxygenEnvName
-        model.properties["envNames"]["agentEnvName"] = agentEnvName
-        envOxygen = NumericalGrid3D(oxygenEnvName, 10,10,10, model)
-        envAgents = ObjectGrid3D(agentEnvName, 10,10,10, model)
-        envOxygen.grid[(5,5,5)] = 1
-        a.add_agent_to_grid(agentEnvName, (5,5,5), model)
+        oxygen_env_name = "oxygenEnv"
+        agent_env_name = "agentEnv"
+        model.properties["envNames"]["oxygenEnvName"] = oxygen_env_name
+        model.properties["envNames"]["agentEnvName"] = agent_env_name
+        env_oxygen = NumericalGrid3D(oxygen_env_name, 10, 10, 10, model)
+        env_agents = ObjectGrid3D(agent_env_name, 10, 10, 10, model)
+        env_oxygen.grid[(5, 5, 5)] = 1
+        a.add_agent_to_grid(agent_env_name, (5, 5, 5), model)
 
         # Checking cancer cells die in low oxygen concentration
         a.step_main(model)
-        self.assertEqual(0, [a for a in envAgents.grid[(5,5,5)] if a.dead].__len__())
+        self.assertEqual(0, [a for a in env_agents.grid[(5, 5, 5)] if
+                             a.dead].__len__())
 
-        self.assertEqual((5,5,5), a.environment_positions[agentEnvName])
+        self.assertEqual((5, 5, 5), a.environment_positions[agent_env_name])
 
-        envOxygen.grid[(5,5,5)] = 0.00001
+        env_oxygen.grid[(5, 5, 5)] = 0.00001
         a.step_main(model)
 
-
-        self.assertEqual(1, [a for a in envAgents.grid[(5,5,5)] if a.dead].__len__())
+        self.assertEqual(1, [a for a in env_agents.grid[(5, 5, 5)] if
+                             a.dead].__len__())
