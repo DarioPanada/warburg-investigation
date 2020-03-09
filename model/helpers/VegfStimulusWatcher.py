99c74c3c1ff711c53e23c54a03f67e3e44f16dc5
Dario Panada <dario.panada@manchester.ac.uk>
2020-03-09 14:55:49 +0000

    Refactor VegfStimulusWatcher

--- model/helpers/VegfStimulusWatcher.py
+++ model/helpers/VegfStimulusWatcher.py
@@ -1,25 +1,26 @@
-'''
-Keeps track of average vegf concentration at blood vessels
-'''
 import numpy as np
 from panaxea.core.Steppables import Helper
 
 
 class VegfStimulusWatcher(Helper, object):
+    '''
+    Keeps track of average vegf concentration at blood vessels
+    '''
 
     def __init__(self, model, endothelialCellNames=["TipCell"]):
         model.output["endothelialCellProperties"] = dict()
         model.output["endothelialCellProperties"]["avgVegf"] = []
-        self.endothelialCellNames = endothelialCellNames
-        self.agentEnvName = model.properties["envNames"]["agentEnvName"]
-        self.vegfEnvName = model.properties["envNames"]["vegfEnvName"]
+        self.endothelial_cell_names = endothelialCellNames
+        self.agent_env_name = model.properties["envNames"]["agentEnvName"]
+        self.vegf_env_name = model.properties["envNames"]["vegfEnvName"]
 
     def step_epilogue(self, model):
-        edothelialCells = [a for a in model.schedule.agents if
-                           a.__class__.__name__ in self.endothelialCellNames]
-        coordinates = [a.environment_positions[self.agentEnvName] for a in
-                       edothelialCells]
-        concentrations = [model.environments[self.vegfEnvName].grid[c] for c in
-                          coordinates]
+        edothelial_cells = [a for a in model.schedule.agents
+                            if a.__class__.__name__ in
+                            self.endothelial_cell_names]
+        coordinates = [a.environment_positions[self.agent_env_name] for a in
+                       edothelial_cells]
+        concentrations = [model.environments[self.vegf_env_name].grid[c]
+                          for c in coordinates]
         model.output["endothelialCellProperties"]["avgVegf"].append(
             np.mean(concentrations))
