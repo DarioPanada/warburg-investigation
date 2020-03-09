fa1850203bd75e9c75ad3f2bfac595c4a0ffabbb
Dario Panada <dario.panada@manchester.ac.uk>
2020-03-09 12:30:23 +0000

    Reformat DeathCauseWatcher

--- model/helpers/ExitConditionWatcher.py
+++ model/helpers/ExitConditionWatcher.py
@@ -1,7 +1,7 @@
 from panaxea.core.Steppables import Helper
 
 '''
-Helper checks if any custom exit condition for the simulation is true and, 
+Helper checks if any custom exit condition for the simulation is true and,
 if so, sets the 'exit' flag of the model
 to true
 '''
@@ -9,14 +9,15 @@ to true
 
 class ExitConditionWatcher(Helper, object):
     '''
-    :param exitConditions - List of functions that, taken an instance of the
+    :param exit_conditions - List of functions that, taken an instance of the
     model, return true or false.
+
     These should be functions, *NOT* lambdas, as pickle has issues with
     lambdas!
     '''
 
-    def __init__(self, exitConditions):
-        self.exitConditions = exitConditions
+    def __init__(self, exit_conditions):
+        self.exit_conditions = exit_conditions
 
     def step_prologue(self, model):
         # It is important this logic is in the prologue and picklers run in
@@ -24,6 +25,6 @@ class ExitConditionWatcher(Helper, object):
         # if the exit flag is set to true, so as to capture the end-state of
         # the model.
 
-        for condition in self.exitConditions:
+        for condition in self.exit_conditions:
             if condition(model):
                 model.exit = True
