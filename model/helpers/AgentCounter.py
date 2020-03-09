394311a078dd4973bccf84082dc129b1c9d207b9
Dario Panada <dario.panada@manchester.ac.uk>
2020-03-09 12:11:24 +0000

    Reformat AgentCounter

--- model/helpers/AgentCounter.py
+++ model/helpers/AgentCounter.py
@@ -7,34 +7,36 @@ Counts number of agent types at each epoch and saves them to model output
 
 class AgentCounter(Helper, object):
 
-    def __init__(self, model, cancerCellClassName="CancerCell"):
-        self.cancerCellClassName = cancerCellClassName
-        self.tipCellClassName = "TipCell"
-        self.trunkCellName = "TrunkCell"
+    def __init__(self, model, cancer_cell_class_name="CancerCell"):
+        self.cancer_cell_class_name = cancer_cell_class_name
+        self.tip_cell_class_name = "TipCell"
+        self.trunk_cell_name = "TrunkCell"
 
-        agentNums = {
+        agent_nums = {
             "cancerCells": [],
             "aliveCancerCells": [],
             "deadCancerCells": [],
             "tipCells": []
         }
 
-        model.output["agentNums"] = agentNums
+        model.output["agentNums"] = agent_nums
 
     def step_epilogue(self, model):
-        cancerCells = len([a for a in model.schedule.agents if
-                           a.__class__.__name__ == self.cancerCellClassName])
-        tipCells = len([a for a in model.schedule.agents if
-                        a.__class__.__name__ == self.tipCellClassName or
-                        a.__class__.__name__ == self.trunkCellName])
-        aliveCancerCells = len([a for a in model.schedule.agents if
-                                a.__class__.__name__ ==
-                                self.cancerCellClassName and not a.dead])
-        deadCancerCells = len([a for a in model.schedule.agents if
-                               a.__class__.__name__ ==
-                               self.cancerCellClassName and a.dead])
+        cancer_cells = len([a for a in model.schedule.agents if
+                            a.__class__.__name__ ==
+                            self.cancer_cell_class_name])
+        tip_cells = len([a for a in model.schedule.agents if
+                         a.__class__.__name__ == self.tip_cell_class_name or
+                         a.__class__.__name__ == self.trunk_cell_name])
+        alive_cancer_cells = len([a for a in model.schedule.agents if
+                                  a.__class__.__name__ ==
+                                  self.cancer_cell_class_name and not a.dead])
+        dead_cancer_cells = len([a for a in model.schedule.agents if
+                                 a.__class__.__name__ ==
+                                 self.cancer_cell_class_name and a.dead])
 
-        model.output["agentNums"]["cancerCells"].append(cancerCells)
-        model.output["agentNums"]["tipCells"].append(tipCells)
-        model.output["agentNums"]["deadCancerCells"].append(deadCancerCells)
-        model.output["agentNums"]["aliveCancerCells"].append(aliveCancerCells)
+        model.output["agentNums"]["cancerCells"].append(cancer_cells)
+        model.output["agentNums"]["tipCells"].append(tip_cells)
+        model.output["agentNums"]["deadCancerCells"].append(dead_cancer_cells)
+        model.output["agentNums"]["aliveCancerCells"].append(
+            alive_cancer_cells)
